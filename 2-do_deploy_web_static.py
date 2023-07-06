a5fae425a99b5a5949244d5ac8b36034a357252d
solomonkassa <solomonmulu000@gmail.com>
2023-07-06 13:20:38 -0700

    all

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,7 +3,7 @@ from fabric.api import *
 import os
 from datetime import datetime
 
-env.hosts = ['35.231.59.48', '35.237.144.156']
+env.hosts = ['54.237.117.138', '100.26.216.97']
 
 
 def do_deploy(archive_path):
