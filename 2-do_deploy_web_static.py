57e0aa04f172e9c8031c56c9a9d71f36e9c31d47
Solomonkassa <118729276+Solomonkassa@users.noreply.github.com>
2023-07-06 23:33:45 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,44 @@
 #!/usr/bin/python3
 from fabric.api import *
 import os
-from datetime import datetime
 
-env.hosts = ['54.237.117.138', '100.26.216.97']
+env.hosts = ['54.237.117.138', '100.26.216.97']  # Replace with your server IP addresses or hostnames
+env.user = 'ubuntu'  # Replace with your username
+env.key_filename = '~/.ssh/school'  # Replace with the path to your SSH private key
 
 
 def do_deploy(archive_path):
-    a_list = archive_path.split(".tgz")
-    archive_wo_ext = "".join(a_list)
-    b_list = archive_wo_ext.split("versions/")
-    archive_wo_ext_ver = "".join(b_list)
-    c_list = archive_path.split("versions/")
-    archive_wo_ver = "".join(c_list)
-    if archive_path:
-        put(archive_path, '/tmp/')
-        run("mkdir -p /data/web_static/releases/{}/".
-            format(archive_wo_ext_ver))
-        run("tar -zxf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_wo_ver, archive_wo_ext_ver))
-        run("rm -r /tmp/{}".format(archive_wo_ver))
-        run("mv /data/web_static/releases/{}/web_static/* \
-        /data/web_static/releases/{}/".format(archive_wo_ext_ver,
-                                              archive_wo_ext_ver))
-        run("rm -rf /data/web_static/releases/{}/web_static".
-            format(archive_wo_ext_ver))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-            format(archive_wo_ext_ver))
-        print("New version deployed!")
-        return True
-    else:
+    if not os.path.exists(archive_path):
         return False
 
-
-def do_pack():
     try:
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        filepath = "versions/web_static_{}.tgz".format(now)
-        local("mkdir -p versions")
-        local("tar -zcvf {} web_static".format(filepath))
-        print("web_static packed: {} -> {}".
-              format(filepath, os.path.getsize(filepath)))
-        return filepath
-    except:
-        return None
+        archive_filename = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_filename)[0]
+
+        # Upload the archive to /tmp/ directory on the web server
+        put(archive_path, '/tmp/')
+
+        # Uncompress the archive to the /data/web_static/releases/ directory
+        run('mkdir -p /data/web_static/releases/{}'.format(archive_no_ext))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(archive_filename, archive_no_ext))
+
+        # Delete the archive from the web server
+        run('rm /tmp/{}'.format(archive_filename))
+
+        # Move the contents of the uncompressed folder to the parent folder
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'
+            .format(archive_no_ext, archive_no_ext))
+        run('rm -rf /data/web_static/releases/{}/web_static'.format(archive_no_ext))
+
+        # Delete the symbolic link /data/web_static/current
+        run('rm -rf /data/web_static/current')
+
+        # Create a new symbolic link to the new version of your code
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_no_ext))
+
+        print("New version deployed!")
+        return True
+    except Exception as e:
+        print("Deployment failed:", str(e))
+        return False
